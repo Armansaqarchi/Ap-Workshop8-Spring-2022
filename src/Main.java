98e7aab37b82f20f8759f19041e460711bc1a34a
Arman sagharchi <99171905+Armansaqarchi@users.noreply.github.com>
2022-04-27 18:36:35 +0430

    5 options including add, remove, export and exist is implemented

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        NoteBook noteBook = NoteBook.getInstance();
 
-
+        noteBook.run();
     }
 }
