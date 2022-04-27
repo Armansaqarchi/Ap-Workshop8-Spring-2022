f1d9834fc1086f991954b98097dafcb5641b1f53
Arman sagharchi <99171905+Armansaqarchi@users.noreply.github.com>
2022-04-27 18:47:09 +0430

    note class added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
-    public static void main(String[] args){
-        NoteBook noteBook = NoteBook.getInstance();
+    public static void main(String[] args) {
 
-        noteBook.run();
     }
 }
